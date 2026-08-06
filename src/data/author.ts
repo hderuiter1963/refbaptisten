7d7465b3f877eeb359ff5d711fb3f42aa604615e
Henk de Ruiter <hderuiter@outlook.com>
2026-08-06 12:53:35 +0200

    Sabbatical-mededeling toevoegen naast auteursfoto

--- src/data/author.ts
+++ src/data/author.ts
@@ -2,4 +2,6 @@ export const author = {
 	name: 'Henk de Ruiter',
 	photo: '/images/home/auteur-henk-de-ruiter.jpg',
 	bio: `Predikant Baptistengemeente 't Lichtpunt te Doetinchem sinds januari 2016. Opgegroeid in de reformatorische gezindte. Studeerde theologie aan de Theologische Universiteit Apeldoorn, Windesheim (GPW), Vrije Universiteit Amsterdam en Baptistenseminarium Amsterdam. Voorheen werkzaam in de ICT-branche als ondernemer. Woonde 6 jaar in Noord-Amerika waar hij met de "Reformed Baptist" in aanraking kwam.`,
+	// Tijdelijke mededeling naast de auteursfoto; laat leeg ('') om te verbergen.
+	note: 'In verband met een sabbatical wordt er momenteel niet aan refbaptisten gewerkt.',
 };
